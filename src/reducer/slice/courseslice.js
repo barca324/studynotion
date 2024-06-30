ba9c28bfc9703f938da74bfb50270781a0642280
32 MK <madhusudan329832@gmail.com>
2024-06-30 14:43:13 +0530

    Initial commit

--- src/reducer/slice/courseslice.js
+++ src/reducer/slice/courseslice.js
@@ -9,7 +9,7 @@ const initialState = {
 
 const courseSlice = createSlice({
     name: "course",
-    initialState, // corrected typo here
+    initialState, 
     reducers: {
         setStep: (state, action) => {
             state.step = action.payload;
